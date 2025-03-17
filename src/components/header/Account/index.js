466f030afe68ce52790dc9cb9e17effdce37f992
Nikita Tepish <nikita.aurum.ma@gmail.com>
2025-03-17 14:37:30 +0400

    second test CI/CD deploy

--- src/components/header/Account/index.js
+++ src/components/header/Account/index.js
@@ -1,33 +1,9 @@
-import './account.scss'
-import { useState, useEffect, createPortal } from 'react'
+import './account.modules.scss'
+import { useState } from 'react'
 
 export default function Account({ closeButton }) {
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
-    const [auth, setAuth] = useState(false)
-
-    async function authentication() {
-        try {
-            const res = await fetch(`https://67cdb91a125cd5af7578970e.mockapi.io/beauty_shop/users`)
-            if (res.ok) {
-                const response = await res.json()
-                const auth = response.filter((i) => i.userName === userName && i.password === password)
-                auth.length ? setAuth(true) : alert('User not found')
-            } else {
-                throw new Error(res.message)
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    useEffect(() => {
-        if (auth) {
-            alert('You are logged in!')
-            closeButton()
-        }
-    }, [auth])
-
 
     return (
         <div className="overwrap">
@@ -38,11 +14,11 @@ export default function Account({ closeButton }) {
                     <p>Please enter your e-mail and password</p>
                     <input type="text" name="e-mail" id="userName" placeholder='Email' onChange={text => setUserName(text.target.value)} value={userName} />
                     <input type="password" name="password" id="password" placeholder='Password' onChange={text => setPassword(text.target.value)} value={password} />
-                    <button onClick={() => authentication()}>Log in</button>
+                    <button>Log in</button>
                     <p id='or'>or</p>
                     <button className="google" >Continue with Google</button>
-                    <p>Forgot your password? <a>Recover password</a></p>
-                    <p>New to Bloom Beauty? <a>Create an account</a></p>
+                    <p>Forgot your password? <button>Recover password</button></p>
+                    <p>New to Bloom Beauty? <button>Create an account</button></p>
                 </div>
             </div>
         </div>
